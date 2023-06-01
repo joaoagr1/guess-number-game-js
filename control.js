5e839d0b01cee8d62a9fdff3b30a0b1219b77e9b
joaoagr1 <joao.rollo1@gmail.com>
2023-05-31 23:05:02 -0300

    commit 1

---
+++ control.js
@@ -0,0 +1,37 @@
+var numeroSortido = Math.floor(Math.random() * 500) + 1;
+      var contErro = 0;
+      var tentativas= 0;
+      var indicadorTentativas=7;
+
+      document.getElementById("numeroForm").addEventListener("submit", function(event) {
+         event.preventDefault(); // Impede o envio do formulário
+
+         var numeroEscolhido = document.getElementById("numeroEscolhido").value;
+
+         if (numeroSortido == numeroEscolhido) {
+            showMessage("Você acertou o número",tentativas);
+            alert("Você acertou o número!!");
+            location.reload(true);
+         } else if (numeroSortido > numeroEscolhido) {
+            tentativas = indicadorTentativas-contErro;
+            showMessage("Digite um número maior!","Tentativas Restantes: " + tentativas);
+            contErro++;
+         } else {
+            tentativas = indicadorTentativas-contErro;
+            showMessage("Digite um número menor!","Tentativas Restantes: " + tentativas);
+            contErro++;
+         }
+
+         if (contErro > indicadorTentativas) {
+            alert("Você gastou todas as suas tentativas... Recomece o jogo!!!");
+            location.reload(true);
+
+         }
+      });
+
+      function showMessage(message,tentativas) {
+         document.getElementById("message").innerHTML = message;
+         document.getElementById("tentativas").innerHTML = tentativas;
+      }
+
+      
